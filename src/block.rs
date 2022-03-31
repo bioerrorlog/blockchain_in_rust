eca16705f37d8b54db8e1a7428d948132fa7c355
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-03-31 09:22:19 +0900

    add block attributes

--- src/block.rs
+++ src/block.rs
@@ -1,8 +1,14 @@
 use std::fmt::{self, Debug, Formatter};
 
+use crate::BlockHash;
+
 pub struct Block {
     pub index: u32,
     pub timestamp: u128,
+    pub hash: BlockHash,
+    pub prev_block_hash: BlockHash,
+    pub nonce: u64,
+    pub payload: String,
 }
 
 impl Debug for Block {
