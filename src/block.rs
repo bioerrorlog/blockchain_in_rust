512ada767ee7091f276162228858311eeb472d1e
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-04-10 19:41:03 +0900

    add bytes

--- src/block.rs
+++ src/block.rs
@@ -2,7 +2,7 @@ use std::fmt::{self, Debug, Formatter};
 
 use crate::hashable::Hashable;
 use crate::BlockHash;
-use crate::{u32_bytes, u64_bytes, u128_bytes};
+use crate::{u128_bytes, u32_bytes, u64_bytes};
 
 pub struct Block {
     pub index: u32,
@@ -46,8 +46,14 @@ impl Block {
 }
 
 impl Hashable for Block {
-    fn bytes (&self) -> Vec<u8> {
+    fn bytes(&self) -> Vec<u8> {
         let mut bytes = vec![];
-        bytes.extend(&)
+        bytes.extend(&u32_bytes(&self.index));
+        bytes.extend(&u128_bytes(&self.timestamp));
+        bytes.extend(&self.prev_block_hash);
+        bytes.extend(&u64_bytes(&self.nonce));
+        bytes.extend(self.payload.as_bytes());
+
+        bytes
     }
 }
