62108c6afa74f4b499a3607b7b614489a3db90da
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-04-16 17:07:28 +0900

    change difficulty in test

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -38,7 +38,7 @@ mod test {
 
     #[test]
     fn add_block_to_chain() {
-        let mut chain = BlockChain::new(0x00000fffffffffffffffffffffffffff);
+        let mut chain = BlockChain::new(0x000fffffffffffffffffffffffffffff);
         let payload = String::from("This is second block.");
         assert_eq!(chain.add_block(payload), Ok(()));
         assert_eq!(chain.blocks.len(), 2);
